1d8128df9c71c0dd0261b88188b93426d40ebeb7
Aya Ibrahim <Aya.Ibrahim@itworx.com>
2022-09-15 11:57:05 +0200

    edit in quantity

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -73,6 +73,9 @@ const ProductDetails: React.FunctionComponent<
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
                   className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  onClick={() => {
+                    setQuantity(quantity + 1);
+                  }}
                 >
                   +
                 </button>
@@ -83,6 +86,13 @@ const ProductDetails: React.FunctionComponent<
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
                   className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  onClick={() => {
+                    if (quantity > 1) setQuantity(quantity - 1);
+                    // else if(quantity == 1){
+                    //   setQuantity(quantity - 1);
+
+                    // }
+                  }}
                 >
                   -
                 </button>
